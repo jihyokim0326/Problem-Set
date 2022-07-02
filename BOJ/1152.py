53db6cbbe2e1b892d72fe97a3b6dd4d636ed9398
jihyokim0326 <tofukim0328@gmail.com>
2022-07-02 10:26:27 +0900

    new codes

--- BOJ/1152.py
+++ BOJ/1152.py
@@ -1,1 +1,5 @@
 # BOJ 1152 : https://www.acmicpc.net/problem/1152
+
+a = list(input().split())
+
+print(len(a))
--- BOJ/2675.py
+++ BOJ/2675.py
@@ -1,1 +1,16 @@
 # BOJ 2675 : https://www.acmicpc.net/problem/2675
+
+num = int(input()) # number of testcases
+
+# alphanumeric = list("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ\$%*+-./:")
+
+for i in range (num):
+    a, b= input().split()
+    a= int(a)
+    b = str(b)
+
+    ans = ''
+    for l in list(b):
+        for j in range(a):
+            print(l, end='')
+    print()
