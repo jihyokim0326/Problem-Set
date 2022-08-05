75952d9fbbb460879caac438b3db2675177eb7a1
jihyokim0326 <tofukim0328@gmail.com>
2022-08-05 19:44:09 +0900

    new codes

--- BOJ/1157.py
+++ BOJ/1157.py
@@ -4,4 +4,23 @@ word = input().upper()
 
 letters = list(word)
 
-print(letters)
+letter_dict = {}
+for a in letters:
+    if a in letter_dict.keys():
+        letter_dict[a] += 1
+    else:
+        letter_dict[a] = 1    
+
+max_num = max(list(letter_dict.values()))
+
+cnt = 0
+ans = None
+for k, v in letter_dict.items():
+    if v == max_num:
+        cnt += 1
+        ans = k
+
+if cnt > 1:
+    print('?')
+else:
+    print(ans)
