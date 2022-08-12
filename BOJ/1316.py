ead720ea636cacd5b9fdfd7f4fd7c1762f83481e
jihyokim0326 <tofukim0328@gmail.com>
2022-08-12 18:59:11 +0900

    new codes

--- BOJ/1316.py
+++ BOJ/1316.py
@@ -1,1 +1,16 @@
 # BOJ : https://www.acmicpc.net/problem/1316
+
+
+Number = int(input())
+result= Number
+
+for i in range(0, Number):
+    word=input()
+    for k in range (0, len(word)-1):
+        if word[k]==word[k+1]:
+            pass
+        elif word[k] in word[k+1:]:
+            result-=1
+
+            break
+print(result)
